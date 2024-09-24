13be8bb8d8e77eca76466aaa066d0cdd95040517
Jonathan Gangi <jgangi@redhat.com>
2024-09-24 16:00:07 -0300

    Improve `dict_merge` from utils module
    Improve `dict_merge` from utils module

--- starmap_client/utils.py
+++ starmap_client/utils.py
@@ -18,8 +18,6 @@ def dict_merge(a: Dict[str, Any], b: Dict[str, Any]) -> Dict[str, Any]:
     Returns:
         dict: A new dictionary with combination of A and B.
     """
-    c = {}
     for x in [a, b]:
         assert_is_dict(x)
-        c.update(x)
-    return c
+    return a | b
